db5d3cf0340c50d58a2de93aca907cc97d544d65
sergioSEa <sergioandreusanchez@gmail.com>
2025-06-18 09:36:52 +0200

    Include function to add species name to sgb

--- Metagenomics/abundance_table_functions.R
+++ Metagenomics/abundance_table_functions.R
@@ -40,3 +40,19 @@ retrieve_taxonomic_level = function(df, taxonomy='t', clean_names=T){
   
   return( list(abundance_table = df, taxonomy_table = complete_taxonomy ) )
 }
+
+
+add_spname_to_sgb = function(output_retrieve_taxonomy){
+  #Make sure input is a list of two
+  if ( length(output_retrieve_taxonomy) != 2 ){ return(NULL) }
+  #Make sure taxonomy level is SGB (t__)
+  df = output_retrieve_taxonomy[[1]]
+  SGBs = grepl('^t__', colnames(df) )
+  if (  sum( SGBs )   == 0  ){ return(NULL) }
+  #Get species names
+  Species_table =  output_retrieve_taxonomy[[2]]
+  colnames(df)[SGBs] %>% sapply( function(x){ Species_table$species[Species_table$SGB== str_split(x, '__')[[1]][2] ]    }  ) -> Sp_names
+  
+  colnames(df)[SGBs] = paste(Sp_names, names(Sp_names), sep = "|")
+  return(df)
+}
